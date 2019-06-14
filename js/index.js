a385663be013a275f53dddd6f5751f100c648e54
vasharma05 <sharmava05@gmail.com>
2019-06-14 13:56:13 +0530

    Next and Prev buttons are now working

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
 var image = document.getElementsByClassName('image-card');
+var next_clicked = false;
+var prev_clicked = false;
         $(document).ready(()=>{
                 $('.image-card').click(()=>{
                     $('#top-layer').toggleClass("disabled");
                 });
-            $('.pic-box').click(()=>{
-                $('#top-layer').toggleClass("disabled");  
+            $('.pic-box').click((e)=>{
+                var next = $('#next')[0];
+                var prev = $('#prev')[0];
+                if(e.target === next || e.target === prev){
+
+                }else{
+                    $('#top-layer').toggleClass("disabled");
+                }
             });
                         
         }); 
@@ -29,9 +37,42 @@ function inner_gallery(subs,total){
         scrollmenu.appendChild(img);
         
     }
-
-    $('.inner-pics').click((e)=>{
+    var inner_pics = $('.inner-pics');
+    inner_pics.click((e)=>{
         $('#slideshow').attr("src", e.target.src);
+    });
+    $('#next').click(()=>{
+        var src = $('#slideshow')[0].currentSrc;
+        
+        for(var i=0;i<total_pics;i++){
+            if(src == inner_pics[i].src){
+                if(i===total_pics-1){
+                    i=0;
+                }else{
+                    i+=1;
+                }
+                $('#slideshow').attr("src", inner_pics[i].src);
+                break;
+            }
+        
+        }
+    });
+
+    $('#prev').click(()=>{
+        var src = $('#slideshow')[0].currentSrc;
+        
+        for(var i=0;i<total_pics;i++){
+            if(src == inner_pics[i].src){
+                if(i===0){
+                    i=total_pics-1;
+                }else{
+                    i-=1;
+                }
+                $('#slideshow').attr("src", inner_pics[i].src);
+                break;
+            }
+        
+        }
     });
 }
 
